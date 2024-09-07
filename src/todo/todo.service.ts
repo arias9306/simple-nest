b18ed3351278960daa33995a124d1812718d633e
Andrés Arias <arias9306@gmail.com>
2024-09-07 14:14:48 -0500

    feat: add missing todo service methods

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepository: Repository<Todo>,
+  ) {}
+
   create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+    return this.todoRepository.create(createTodoDto);
   }
 
   findAll() {
-    return `This action returns all todo`;
+    return this.todoRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} todo`;
+    return this.todoRepository.findOne({ where: { id } });
   }
 
   update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+    return this.todoRepository.update(id, updateTodoDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} todo`;
+    return this.todoRepository.delete(id);
   }
 }
